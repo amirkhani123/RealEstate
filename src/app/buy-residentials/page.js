7791afbc477aa35226013ebee266e22e7f1f591e
amirKhani <ww.amirkhani.i@gmail.com>
2025-03-04 19:46:50 +0330

    ssg adsPage

--- src/app/buy-residentials/page.js
+++ src/app/buy-residentials/page.js
@@ -1,7 +1,8 @@
 import BuyResidentialsPage from "@/components/templates/BuyResidentials";
 async function BuyResidentials({ searchParams }) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/advertisements`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/advertisements`,
+    { cache: "force-cache" }
   );
   const data = await res.json();
   if (data.profiles) {
